23ccfcfbd15ce21e5c8dea6f9b407d6bbb5353f6
mariogavilanl <mariogavilan.l@gmail.com>
2016-04-26 08:00:50 -0300

    Variable de entorno
    descomentar .env en .gitignore para poder clonar el proyecto completo

--- resources/views/faq.blade.php
+++ resources/views/faq.blade.php
@@ -8,7 +8,7 @@
                 <div class="panel-heading">Ultimos registros</div>
 
                 <div class="panel-body">
-                    lista de registros
+                    lista de registros 0000
                 </div>
             </div>
         </div>
