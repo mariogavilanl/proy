3bcc7ae601e9d0024116701a168f596ac226081d
mario <mariogavilan.l@gmail.com>
2016-05-01 10:17:13 -0300

    Creacion de migracion bitacora
    creacion de igracion itacora igual a la que existe

--- database/migrations/2016_04_23_234546_create_bitacora_table.php
+++ database/migrations/2016_04_23_234546_create_bitacora_table.php
@@ -13,11 +13,25 @@ class CreateBitacoraTable extends Migration
     public function up()
     {
 
+
         Schema::create('bitacora', function (Blueprint $table) {
+            
             $table->increments('id');
-            $table->string('name');
-            $table->string('email');
-            $table->string('password');
+            $table->date('fecha_solicitud');
+            $table->string('quien_soliita');
+            $table->string('via_de_solicitud');
+            $table->dateTime('hora_de_solicitud');
+            $table->longText('descripcion');
+            $table->string('usuario');
+            $table->string('anexo');
+            $table->string('piso');
+            $table->string('area');
+            $table->string('estado');
+            $table->longText('solucion');
+            $table->boolean('pendiente');
+            $table->string('tecnico_responsable');
+            $table->date('fecha_termino');
+            // llaves foraneas
             $table->integer('user_id')->unsigned();
             $table->foreign('user_id')->references('id')->on('users');
             $table->rememberToken();
